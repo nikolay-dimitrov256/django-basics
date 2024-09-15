0206341935c83693cbd04645c6279e855afb52e7
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-09-15 19:50:10 +0300

    Updated settings.py

--- djangoIntroduction/djangoIntroduction/settings.py
+++ djangoIntroduction/djangoIntroduction/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'djangoIntroduction.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "todo_db",
+        "USER": "postgres",
+        "PASSWORD": "admin",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
