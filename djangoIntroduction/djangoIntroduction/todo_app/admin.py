83e51f327ff31e82295c8fad8dba6a40d6b81ad2
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-09-15 22:13:02 +0300

    Completed project

--- djangoIntroduction/djangoIntroduction/todo_app/admin.py
+++ djangoIntroduction/djangoIntroduction/todo_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from djangoIntroduction.todo_app.models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
--- djangoIntroduction/djangoIntroduction/todo_app/views.py
+++ djangoIntroduction/djangoIntroduction/todo_app/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from djangoIntroduction.todo_app.models import Task
+
+
+def index(request):
+    title_filter = request.GET.get('title_filter', '')
+
+    tasks = Task.objects.filter(name__icontains=title_filter)
+
+    context = {
+        'tasks': tasks,
+        'title_filter': title_filter,
+    }
+
+    return render(request, 'todo/index.html', context)
