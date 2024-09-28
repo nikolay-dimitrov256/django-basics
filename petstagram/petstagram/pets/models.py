ebe8b7a6a04607a822ab9493f751ce09357f9115
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-09-28 13:58:18 +0300

    feat: Created Pet model

--- petstagram/petstagram/pets/models.py
+++ petstagram/petstagram/pets/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+from petstagram.accounts.models import User
+
+
+class Pet(models.Model):
+    name = models.CharField(
+        max_length=30,
+    )
+
+    pet_photo = models.URLField()
+
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    slug = models.CharField(
+        max_length=100,
+        blank=True,
+    )
+
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='pets',
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
