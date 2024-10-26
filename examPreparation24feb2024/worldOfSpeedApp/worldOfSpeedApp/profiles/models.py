276cce007e5aae726e9600a7e00becdb80cdeb2c
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-10-26 16:57:20 +0300

    feat: Created Profile model

--- examPreparation24feb2024/worldOfSpeedApp/worldOfSpeedApp/profiles/models.py
+++ examPreparation24feb2024/worldOfSpeedApp/worldOfSpeedApp/profiles/models.py
@@ -1,3 +1,42 @@
+from django.core.validators import MinLengthValidator, RegexValidator, MinValueValidator, MaxLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    username = models.CharField(
+        max_length=15,
+        validators=[
+            MinLengthValidator(3, message='Username must be at least 3 chars long!'),
+            RegexValidator(regex=r'^\w+$', message='Username must contain only letters, digits, and underscores!'),
+        ]
+    )
+
+    email = models.EmailField()
+
+    age = models.IntegerField(
+        validators=[
+            MinValueValidator(21)
+        ],
+        help_text='Age requirement: 21 years and above.',
+    )
+
+    password = models.CharField(
+        max_length=20,
+    )
+
+    first_name = models.CharField(
+        max_length=25,
+        null=True,
+        blank=True,
+    )
+
+    last_name = models.CharField(
+        max_length=25,
+        null=True,
+        blank=True,
+    )
+
+    profile_picture = models.URLField(
+        null=True,
+        blank=True,
+    )
