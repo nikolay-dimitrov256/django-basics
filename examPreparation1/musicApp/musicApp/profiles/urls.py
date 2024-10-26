5cca93078f419892d5d29d05bb86d7a48bd5df6e
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-10-26 14:15:22 +0300

    feat: Developed Delete profile page

--- examPreparation1/musicApp/musicApp/profiles/urls.py
+++ examPreparation1/musicApp/musicApp/profiles/urls.py
@@ -3,4 +3,5 @@ from musicApp.profiles import views
 
 urlpatterns = [
     path('details/', views.ProfileDetailsView.as_view(), name='profile-details'),
+    path('delete/', views.ProfileDeleteView.as_view(), name='delete-profile'),
 ]
--- examPreparation1/musicApp/musicApp/profiles/views.py
+++ examPreparation1/musicApp/musicApp/profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, DeleteView
 
 from musicApp.common.utils import get_profile
 from musicApp.profiles.models import Profile
@@ -8,6 +9,15 @@ from musicApp.profiles.models import Profile
 class ProfileDetailsView(DetailView):
     model = Profile
     template_name = 'profiles/profile-details.html'
+
+    def get_object(self, queryset=None):
+        return get_profile()
+
+
+class ProfileDeleteView(DeleteView):
+    model = Profile
+    template_name = 'profiles/profile-delete.html'
+    success_url = reverse_lazy('home')
 
     def get_object(self, queryset=None):
         return get_profile()
