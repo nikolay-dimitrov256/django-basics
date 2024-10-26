969d45109c09eb1c68b7cfd06f981438fc8c6027
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-10-26 17:45:32 +0300

    feat: Developed Index page

---
+++ examPreparation24feb2024/worldOfSpeedApp/worldOfSpeedApp/common/helpers.py
@@ -0,0 +1,5 @@
+from worldOfSpeedApp.profiles.models import Profile
+
+
+def get_profile():
+    return Profile.objects.first()
--- examPreparation24feb2024/worldOfSpeedApp/worldOfSpeedApp/common/views.py
+++ examPreparation24feb2024/worldOfSpeedApp/worldOfSpeedApp/common/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from worldOfSpeedApp.common.helpers import get_profile
+
+
+class IndexView(TemplateView):
+    template_name = 'common/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile'] = get_profile()
+
+        return context
