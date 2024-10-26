43785074bdced8590be31f9edb3a482adea50032
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-10-26 10:06:28 +0300

    feat: Developed Home page

--- examPreparation1/musicApp/musicApp/common/views.py
+++ examPreparation1/musicApp/musicApp/common/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, FormView
+from django.views.generic.edit import BaseFormView
 
-# Create your views here.
+from musicApp.albums.models import Album
+from musicApp.common.utils import get_profile
+from musicApp.profiles.forms import ProfileCreateForm
+from musicApp.profiles.models import Profile
+
+
+class HomeView(ListView, BaseFormView):
+    model = Album
+    context_object_name = 'albums'
+    form_class = ProfileCreateForm
+    success_url = reverse_lazy('home')
+
+    def get_template_names(self):
+        profile = get_profile()
+
+        if profile is None:
+            return ['common/home-no-profile.html']
+
+        return ['common/home-with-profile.html']
+
+    def get_context_data(self, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=None, **kwargs)
+        context['profile'] = get_profile()
+
+        return context
+
+    def form_valid(self, form):
+        form.save()
+
+        return super().form_valid(form)
---
+++ examPreparation1/musicApp/musicApp/profiles/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from musicApp.profiles.models import Profile
+
+
+class ProfileBaseForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+            'age': forms.NumberInput(attrs={'placeholder': 'Age'}),
+        }
+
+
+class ProfileCreateForm(ProfileBaseForm):
+    pass
